0dc03ed0f40d96094ba5ad6b3756683ab6f571cd
MateusGustavo22 <githubmateus@gmail.com>
2024-02-16 22:09:26 -0300

    vscode settings

---
+++ index.js
@@ -0,0 +1,11 @@
+const settings = {
+  "editor.fontSize": 14,
+  "editor.lineHeight": 1.6,
+  "editor.minimap.enabled": false,
+  "editor.semanticHighlighting.enabled": false,
+  "breadcrumbs.enabled": false,
+  "workbench.statusBar.visible": false,
+  "explorer.compactFolders": false
+}
+
+// Ocultar: outline, timeline, split-editor-right
